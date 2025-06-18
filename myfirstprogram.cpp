c00b8768755be278ada4876741417d1d5a28c4ff
Harshit Sethi <harshitrsethi@gmail.com>
2025-06-18 21:24:23 +0530

    Adding Day 1 and trying to ignore files

---
+++ myfirstprogram.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    /* The code below is my first time proper C++ code 
+    so its great no matter what you say*/
+    cout << "Hello World! \n";
+    // This is a comment 
+    cout << "Harshit is learning C++ before college \n";
+    cout << 3 << endl;
+    cout << 3+3 << endl;
+    cout << "\\";
+    return 0;
+}
