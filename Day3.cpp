cbb64fdd4ce49550143810cb3cd326ec1f6102ec
Harshit Sethi <harshitrsethi@gmail.com>
2025-06-20 15:11:26 +0530

    Learned User Input

---
+++ Day3.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    //Learning C++ User Input
+
+    int x;
+    cout << "Type a number";
+    cin >> x;
+    cout << "Your number is " << x << endl;
+    cout << "It worked YES, let's try to create a simpe addition machine \n";
+    int a,b,c;
+    cout << "Welcome to my simple addition machine in which you can add upto 3 integers \n";
+    cout << "Type first No. \n";
+    cin >> a;
+    cout << "Your first No. was " << a << endl;
+    cout << "Type second No. \n";
+    cin >> b;
+    cout << "Your secound No. was " << b << endl;
+    cout << "Type third No. \n";
+    cin >> c;
+    cout << "Your third No. was " << c << endl;
+    int sum = a+b+c;
+    cout << "Your sum is " << sum << ". \n";
+
+    //End of C++ User Input
+
+    //Learning Data Types
+
+return 0;
+}
